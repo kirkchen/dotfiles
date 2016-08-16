7435fb30babde9a7a2559f2cfd27361284eca9dd
Kirk Chen <kirkchen@kkbox.com>
2016-08-16 11:03:24 +0800

    Initial project

---
+++ auto-install.sh
@@ -0,0 +1,24 @@
+VIMHOME=~/.vim
+
+warn() {
+	echo "$1" >&2
+}
+die() {
+	warn "$1"
+	exit 1
+}
+
+[ -e "$VIMHOME/vimrc" ] && die "$VIMHOME/vimrc already exists."
+[ -e "~/.vim" ] && die "~/.vim already exists."
+[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
+
+git clone git://github.com/kirkchen/vimrc.git "$VIMHOME"
+
+cd "$VIMHOME"
+
+git submodule update --init
+
+cd ..
+ln -s .vim/vimrc .vimrc
+
+echo "kirkchen's vimrc is installed."
---
+++ vimrc
@@ -0,0 +1,19 @@
+"compatible with vi
+set nocompatible
+
+"show line number
+set number
+
+"show syntax highlight
+syntax on
+
+set showmatch
+
+set encoding=utf-8
+
+set tabstop=4
+set softtabstop=4
+
+set nobackup
+
+colorscheme desert
