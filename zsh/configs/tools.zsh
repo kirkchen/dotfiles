aed9d3cba471748308152fc1c5bf80786faa4b9f
Kirk Chen <kirk.chen@jkopay.com>
2025-06-15 12:39:23 +0800

    setup new tools

--- zsh/configs/tools.zsh
+++ zsh/configs/tools.zsh
@@ -17,3 +17,34 @@ export NVM_DIR="$HOME/.nvm"
 
 # Jetbrains
 [[ -d "$HOME/.jetbrains" ]] && export PATH="$HOME/.jetbrains:$PATH"
+
+# GVM
+[[ -s "/Users/kirk.chen/.gvm/scripts/gvm" ]] && source "/Users/kirk.chen/.gvm/scripts/gvm"
+
+# dotnet
+[[ -d "$HOME/.dotnet" ]] && export PATH="$HOME/.dotnet:$PATH"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/homebrew/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# Windsurf
+[[ -d "/Users/kirk.chen/.codeium/windsurf/bin" ]] && export PATH="/Users/kirk.chen/.codeium/windsurf/bin:$PATH"
+
+# pnpm
+export PNPM_HOME="/Users/kirk.chen/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
